86229b52d5a68a4613d079e154666573ad3404f9
Ekta <eprashnani@ucsb.edu>
2018-06-13 18:21:22 -0700

    added download scripts

---
+++ scripts/download_PieAPPv0.1_PT_weights.sh
@@ -0,0 +1,4 @@
+
+mkdir weights
+
+wget https://ece.ucsb.edu/~ekta/projects/CVPR2018_PieAPP/weights/PieAPPv0.1.pth --no-check-certificate
---
+++ scripts/download_PieAPPv0.1_TF_weights.sh
@@ -0,0 +1,7 @@
+
+mkdir weights
+
+wget https://ece.ucsb.edu/~ekta/projects/CVPR2018_PieAPP/weights/PieAPPv0.1_TF.tar.gz --no-check-certificate
+
+tar -xzf PieAPPv0.1_TF.tar.gz -C weights
+rm PieAPPv0.1_TF.tar.gz
