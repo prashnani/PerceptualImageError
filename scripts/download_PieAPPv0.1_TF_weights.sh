dbd493d591d6963b446a0420fc5341fdc98557a0
Ekta <eprashnani@ucsb.edu>
2018-06-20 09:44:53 -0600

    Updated tensorflow download script

--- scripts/download_PieAPPv0.1_TF_weights.sh
+++ scripts/download_PieAPPv0.1_TF_weights.sh
@@ -1,7 +1,7 @@
 
 mkdir weights
 
-wget https://ece.ucsb.edu/~ekta/projects/CVPR2018_PieAPP/weights/PieAPPv0.1_TF.tar.gz --no-check-certificate
+wget https://web.ece.ucsb.edu/~ekta/projects/PieAPPv0.1/weights/PieAPPv0.1_TF.tar.gz --no-check-certificate
 
 tar -xzf PieAPPv0.1_TF.tar.gz -C weights
 rm PieAPPv0.1_TF.tar.gz
